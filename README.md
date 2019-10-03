d56b65d181ccddb33c700a50e7cd667ff6024956
Juno Bang <juno@coworksys.com>
2019-10-03 16:14:28 +0900

    End

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Movie App 2019
 
 React JS Fundamentals Course(2019 update!)
+
+setState 할 때 마다 render()를 호출 = 즉, state가 변경되면 화면이 변경됨
--- src/App.js
+++ src/App.js
@@ -1,38 +1,65 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import axios from 'axios'
+
+import Movie from './Movie'
+import './App.css'
+
+
 
 class App extends React.Component {
   state = {
-    count : 0
+    isLoading: true,
+    movies: []
   }
 
-  add = () => {
-    console.log("add")
-    this.setState(current => ({ count: current.count+100000000 }))
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    this.setState({ movies, isLoading: false })
+    }
+
+  componentDidMount() {
+    console.log("Did mount")
+    this.getMovies();
+  }
+  componentDidUpdate() {
+
+  }
+  componentWillUnmount() {
+
   }
 
-  minus = () => {
-    console.log("minus")
-    this.setState(current => ({ count: current.count-1 }))
-  }
-
-  zero = () => {
-    console.log("zero")
-    this.setState(current => ({ count: 0 }))
-  }
-
-  render() {
+  render() 
+  {
+    const { isLoading, movies } = this.state
+    console.log("render")
     return (
-      <div>
-        <h1> Number is : {this.state.count} </h1>
-        <button onClick={this.add}>Add</button>
-        <button onClick={this.minus}>Minus</button>
-        <button onClick={this.zero}>zero</button>
-      </div>
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+              <span className="loader__text">Loading...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                key = {movie.id}
+                id = {movie.id}
+                year = {movie.year}
+                title = {movie.title}
+                summary = {movie.summary}
+                poster = {movie.medium_cover_image}
+                genres = {movie.genres}
+              />  
+            ))}
+          </div>
+        )}
+      </section>
     )
   }
 }
-
-
 
 export default App;
